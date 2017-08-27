d8afa2cc1d7bc4e62ca91f7aad8ea03f2947418d
valerakozhanov <valerykozhanov@gmail.com>
2017-08-27 19:10:30 +0300

    passing data with router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,44 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PagesComponent } from './pages/pages.component';
+import {Routes, RouterModule} from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+
+
+const appRoutes: Routes = [
+  { path: '',
+    component: Page1Component,
+    data: { title: 'Heroes List' }},
+  {
+    component: Page2Component,
+    path: 'p2/:id',
+    data:{ dataParam: 'param from coonfig'}
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PagesComponent
+    PagesComponent,
+    PageNotFoundComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    RouterModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
+
+//dok
+//https://angular.io/api/router/
